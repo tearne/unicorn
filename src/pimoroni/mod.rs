a9aba398c4ae389957d056cf9e12c64bc35ac192
tearne <tearne@gmail.com>
2021-09-10 19:45:36 +0000

    RGB::BLACK

--- src/pimoroni/mod.rs
+++ src/pimoroni/mod.rs
@@ -8,6 +8,12 @@ pub struct RGB {
     pub b: u8,
 }
 impl RGB {
+    pub const BLACK: RGB = RGB{
+        r:0,
+        g:0,
+        b:0
+    };
+    
     pub fn new(r: u8, g: u8, b: u8) -> Self {
         RGB { r, g, b }
     }
