780a8c44eef94e2e057de5b51868eed4926f8f58
tearne <tearne@gmail.com>
2021-09-11 06:10:48 +0000

    Const again

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,6 @@ pub mod error;
 pub mod keyboard;
 pub mod pimoroni;
 
-pub static BLACK: RGB = RGB { r: 0, g: 0, b: 0 };
-
 #[derive(Debug, Clone, Copy)]
 pub struct RGB {
     pub r: u8,
@@ -11,6 +9,8 @@ pub struct RGB {
     pub b: u8,
 }
 impl RGB {
+    pub const BLACK: RGB = RGB { r: 0, g: 0, b: 0 };
+
     pub fn new(r: u8, g: u8, b: u8) -> Self {
         RGB { r, g, b }
     }
